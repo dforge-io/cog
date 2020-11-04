32395a46d543092a1b8cfbea15e1f275bb8bfe2d
William Cheong Weelau <cheongweelau@gmail.com>
2020-11-05 00:05:46 +0800

    added messenger hook

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, Logger, NotFoundException, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  private logger: Logger;
+
+  constructor(
+    private readonly appService: AppService
+  ) {
+    this.logger = new Logger(AppController.name);
+  }
 
   @Get()
   getHello(): string {
@@ -13,5 +20,40 @@ export class AppController {
   @Post('whatsapp/:number')
   sendWhatsapp(@Param() params) {
     this.appService.send(params.id);
+  }
+
+  @Post('webhook')
+  webhook(@Body() body) {
+    if(body.object ==='page'){
+      body.entry.forEach(entry => {
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+
+      return 'EVENT_RECEIVED';    
+    }
+    else {
+      throw new NotFoundException();
+    }
+  }
+
+  @Get('webhook')
+  getWebhook(@Query() query) {
+    let VERIFY_TOKEN = "DFORGE_TOKEN";
+    let mode = query['hub.mode'];
+    let token = query['hub.verify_token'];
+    let challenge = query['hub.challenge'];
+
+    this.logger.log(`${mode}, ${token}`)
+    if(mode && token){
+      if( mode === 'subscribe' && token === VERIFY_TOKEN) {
+        this.logger.log('WEBHOOK_VERIFIED');
+        return challenge;
+      }
+      else {
+        this.logger.log('forbidden!')
+        throw new ForbiddenException();
+      }
+    }
   }
 }
